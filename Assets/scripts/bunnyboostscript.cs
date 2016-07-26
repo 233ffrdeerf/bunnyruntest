ac2b42f1dca40c901b035e36477d110eea1fede7
233ffrdeerf <kiwiboy2225@gmail.com>
2016-07-27 01:22:32 +1200

    added animation jump

--- Assets/scripts/bunnyboostscript.cs
+++ Assets/scripts/bunnyboostscript.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class bunnyboostscript : MonoBehaviour
 {
     private Rigidbody2D myRigidBody;
+    private Animator myAnim;
     public float bunnyjumpforce = 500f;
     // Use this for initialization
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,6 +21,7 @@ public class bunnyboostscript : MonoBehaviour
             myRigidBody.AddForce(transform.up * bunnyjumpforce);
 
         }
+        myAnim.SetFloat("thetransition", myRigidBody.velocity.y);
     }
 }
  
